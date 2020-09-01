f642d7a914db07303e0ef80712966087bf019d6c
fathur <vathroh@gmail.com>
2020-09-01 15:27:54 +0700

    revisi skor kpp -11

--- app/Http/Controllers/kppController.php
+++ app/Http/Controllers/kppController.php
@@ -507,6 +507,9 @@ class kppController extends Controller
                     (select skor_kpp.scor from skor_kpp where skor_kpp.items = "surat_keputusan" AND skor_kpp.criteria = kppdatas.surat_keputusan) as sk
                     '),
             \DB::raw('
+            CASE
+            WHEN 
+                IF(
                     CASE
                         WHEN
                     (select skor_kpp.scor from skor_kpp where skor_kpp.items = "anggaran_dasar" AND skor_kpp.criteria = kppdatas.anggaran_dasar) = 1 THEN 1 ELSE 0 END 
@@ -515,7 +518,16 @@ class kppController extends Controller
                         WHEN
                     (select skor_kpp.scor from skor_kpp where skor_kpp.items = "anggaran_rumah_tangga" AND skor_kpp.criteria = kppdatas.anggaran_rumah_tangga) = 1 THEN 1 ELSE 0 END
                 +
-                    (select skor_kpp.scor from skor_kpp where skor_kpp.items = "surat_keputusan" AND skor_kpp.criteria = kppdatas.surat_keputusan) as skr_awal
+                    (select skor_kpp.scor from skor_kpp where skor_kpp.items = "surat_keputusan" AND skor_kpp.criteria = kppdatas.surat_keputusan)
+                    > 1, 2, 0
+                )
+                +
+                IF(
+                    (select skor_kpp.scor from skor_kpp where skor_kpp.items = "struktur_organisasi" AND skor_kpp.criteria = kppdatas.struktur_organisasi) = 2, 2, 0
+                )
+
+
+                   as skr_awal
                 '),
             \DB::raw('
                     (select skor_kpp.scor from skor_kpp where skor_kpp.items = "struktur_organisasi" AND skor_kpp.criteria = kppdatas.struktur_organisasi) as skr_str

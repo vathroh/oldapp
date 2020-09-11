21129831e9b882cc4ba2edfa2899988fdc776df9
fathur <vathroh@gmail.com>
2020-09-11 09:21:33 +0700

    revisi minor title

--- resources/views/layouts/bickery.blade.php
+++ resources/views/layouts/bickery.blade.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 <html>
 	<head>
-		<title>OSP-11</title>
+		<title>OSP-1</title>
 		<meta charset="utf-8">
 		<meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no">
 		<script src="{{ asset('js/app.js') }}" defer></script>

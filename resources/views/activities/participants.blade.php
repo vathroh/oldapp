652079937dfa5e2cbfbd5ecc5b50d6d1ab788b8e
fathur <vathroh@gmail.com>
2020-11-25 09:10:12 +0700

    kegiatan monitoringi3

--- resources/views/activities/participants.blade.php
+++ resources/views/activities/participants.blade.php
@@ -23,7 +23,7 @@
 				<tr>
 					<th scope="row">{{ $loop->iteration }}</th>
 					<td>{{ $participant->user()->first()->name }}</td>
-					<td>{{ $participant->user()->first()->posisi()->first()->job_title }}</td>
+					<td>{{ $participant->user()->first()->posisi()->first()->job_title ?? "" }}</td>
 					<td>{{ $participant->user()->first()->jobDesc()->first()
 					->kabupaten()->first()->NAMA_KAB ?? 'OSP'}}</td>
 				</tr>

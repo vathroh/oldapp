e3a54f0c2bf312fa1c04a60a24989fe672ef6264
fathur <vathroh@gmail.com>
2020-12-11 12:49:31 +0700

    update routes di web.php

--- routes/web.php
+++ routes/web.php
@@ -267,3 +267,6 @@ Route::get('personnel-evaluation-user-create', 'personnelEvaluation\evaluation@u
 Route::get('personnel-evaluation-home', 'personnelEvaluation\evaluation@ajaxHome');
 Route::get('search-aspect-id', 'personnelEvaluation\setup@ajaxAspect');
 Route::get('ajax-personnel-evaluation-upload', 'personnelEvaluation\upload@ajaxUploadFile');
+
+//GIS
+Route::resource('gis', 'gis\gisController');

f4b57e5f82b467295300fadd8984d3c55430fde2
vathroh <vathroh@gmail.com>
2021-04-12 14:14:08 +0700

    part 2: repair amount of fasilitator on homeassessor  personnel evaluation page

--- app/Http/Controllers/personnelEvaluation/assessor/showFaskelPersonnelsController.php
+++ app/Http/Controllers/personnelEvaluation/assessor/showFaskelPersonnelsController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers\personnelEvaluation\assessor;
 
 use App\User;
 use App\job_desc;
+use Carbon\Carbon;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 use Illuminate\Support\Facades\Auth;
@@ -16,6 +17,15 @@ class showFaskelPersonnelsController extends Controller
     {
         $this->middleware('auth');
     }
+    
+    public function job_desc()
+	{
+		$lastYear 					= personnel_evaluation_setting::max('year');
+		$lastQuarter 				= personnel_evaluation_setting::where('year', $lastYear)->max('quarter');
+		
+		return job_desc::withoutGlobalScopes()->selectRaw('*, UNIX_TIMESTAMP(starting_date) as starting_timestamp, UNIX_TIMESTAMP(finishing_date) as finishing_timestamp')->get();
+		
+	}
 
     public function setting($jobId)
     {
@@ -26,24 +36,31 @@ class showFaskelPersonnelsController extends Controller
     }
 
     public function users($district)
-    {
-        $myZones        = explode(", ", Auth::user()->areaKerja->pluck('zone')->first());
-        $users          = User::find(job_desc::join('work_zones', 'work_zones.id', '=', 'job_descs.work_zone_id')->where('district', $district)->pluck('user_id'));
-        return $users;
+    {        
+		$lastYear = personnel_evaluation_setting::max('year');
+		$myZones = explode(", ", Auth::user()->areaKerja->pluck('zone')->first());
+		$lastQuarter = personnel_evaluation_setting::where('year', $lastYear)->max('quarter');		
+		$dt = $lastYear . '-' . $lastQuarter*3 .'-' . 1;
+		$time = Carbon::parse($dt)->timestamp;	
+	
+		$job_desc          = job_desc::withoutGlobalScopes()->join('work_zones', 'work_zones.id', '=', 'job_descs.work_zone_id')->where('district', $district)->selectRaw('*, UNIX_TIMESTAMP(starting_date) as starting_timestamp, UNIX_TIMESTAMP(finishing_date) as finishing_timestamp')->get();
+        
+        return $job_desc->where('starting_timestamp', '<=', $time)->where('finishing_timestamp', '>', $time);
+        
     }
 
     public function allpersonnels($jobId, $district)
     {
-        $users   = $this->users($district);
+        $job_descs   = $this->users($district);
         $lastSetting    = $this->setting($jobId);
-        return view('personnelEvaluation.assessor.personnels.allpersonnels', compact(['users', 'lastSetting']));
+        return view('personnelEvaluation.assessor.personnels.allpersonnels', compact(['job_descs', 'lastSetting']));
     }
 
     public function belumMengisi($jobId, $district)
     {
         $users          = $this->users($district);
         $lastSetting    = $this->setting($jobId);
-        $personnels     = $this->setting($jobId)->jobDesc->whereIn('user_id', $users->pluck('id'))->whereNotIn('user_id', $this->setting($jobId)->evaluationValue->pluck('userId'));
+        $personnels     = $this->setting($jobId)->jobDesc->whereIn('user_id', $users->pluck('user_id'))->whereNotIn('user_id', $this->setting($jobId)->evaluationValue->pluck('userId'));
         return view('personnelEvaluation.assessor.personnels.belumMengisi', compact(['users', 'personnels', 'lastSetting']));
     }
 
@@ -51,15 +68,15 @@ class showFaskelPersonnelsController extends Controller
     {
         $users          = $this->users($district);
         $lastSetting    = $this->setting($jobId);
-        $values         = personnel_evaluation_value::where('settingId', $lastSetting->id)->whereIn('userId', $users->pluck('id'))->where('ok_by_user', 1)->get();
+        $values         = personnel_evaluation_value::where('settingId', $lastSetting->id)->whereIn('userId', $users->pluck('user_id'))->where('ok_by_user', 1)->get();
         return view('personnelEvaluation.assessor.personnels.selesaiMengisi', compact(['users', 'values', 'lastSetting']));
     }
 
     public function prosesMengisi($jobId, $district)
     {
-        $users          = $this->users($district);
+		$users          = $this->users($district);
         $lastSetting    = $this->setting($jobId);
-        $values         = personnel_evaluation_value::where('settingId', $lastSetting->id)->whereIn('userId', $users->pluck('id'))->where('ok_by_user', 0)->get();
+        $values         = personnel_evaluation_value::where('settingId', $lastSetting->id)->whereIn('userId', $users->pluck('user_id'))->where('ok_by_user', 0)->get();
         return view('personnelEvaluation.assessor.personnels.prosesMengisi', compact(['users', 'values', 'lastSetting']));
     }
 
@@ -68,7 +85,7 @@ class showFaskelPersonnelsController extends Controller
 		
         $users          = $this->users($district);
         $lastSetting    = $this->setting($jobId);
-        $values         = personnel_evaluation_value::where('settingId', $lastSetting->id)->whereIn('userId', $users->pluck('id'))->where('ok_by_user', 1)->where('totalScore', '0.00')->get();
+        $values         = personnel_evaluation_value::where('settingId', $lastSetting->id)->whereIn('userId', $users->pluck('user_id'))->where('ok_by_user', 1)->where('totalScore', '0.00')->get();
         return view('personnelEvaluation.assessor.personnels.siapEvaluasi', compact(['users', 'values', 'lastSetting']));
     }
 
@@ -76,7 +93,7 @@ class showFaskelPersonnelsController extends Controller
     {
         $users          = $this->users($district);
         $lastSetting    = $this->setting($jobId);
-        $values         = personnel_evaluation_value::where('settingId', $lastSetting->id)->whereIn('userId', $users->pluck('id'))->where('ok_by_user', 1)->where('totalScore', '!=', '0.00')->where('ready', 0)->get();
+        $values         = personnel_evaluation_value::where('settingId', $lastSetting->id)->whereIn('userId', $users->pluck('user_id'))->where('ok_by_user', 1)->where('totalScore', '!=', '0.00')->where('ready', 0)->get();
         return view('personnelEvaluation.assessor.personnels.prosesEvaluasi', compact(['users', 'values', 'lastSetting']));
     }
 
@@ -84,7 +101,7 @@ class showFaskelPersonnelsController extends Controller
     {
         $users          = $this->users($district);
         $lastSetting    = $this->setting($jobId);
-        $values         = personnel_evaluation_value::where('settingId', $lastSetting->id)->whereIn('userId', $users->pluck('id'))->where('ok_by_user', 1)->where('totalScore', '!=', '0.00')->where('ready', 1)->get();
+        $values         = personnel_evaluation_value::where('settingId', $lastSetting->id)->whereIn('userId', $users->pluck('user_id'))->where('ok_by_user', 1)->where('totalScore', '!=', '0.00')->where('ready', 1)->get();
 
         return view('personnelEvaluation.assessor.personnels.selesaiEvaluasi', compact(['users', 'values', 'lastSetting']));
     }
